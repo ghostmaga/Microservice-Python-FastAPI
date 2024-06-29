757226abe641e00019ea29a7734779e9e2b4bb6a
ghostmaga <magzhan.khorshat@bk.ru>
2024-06-29 13:47:51 +0500

    movie service

---
+++ movie_service/app/main.py
@@ -0,0 +1,18 @@
+﻿#~/movie-service/app/main.py
+from fastapi import FastAPI # type: ignore
+from app.api.movies import movies
+from app.api.db import metadata, database, engine
+
+metadata.create_all(engine)
+
+app = FastAPI()
+
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
+
+app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
